5732de8950fcc025b891c5d29c5e2541d467d0f3
TopWork0ut <olegiksen@gmail.com>
2022-05-27 09:34:28 +0300

    modified main.py with deleted str methods

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from classes.publications import PrintedBook, AudioBook, Magazine
 
 def main():
     printed_book = PrintedBook("PrintedBookCaption", False, "Fantasy", 200, "Coated Paper")
-    print(printed_book.__str__())
+    print(printed_book)
 
     audio_book = AudioBook("AudioBookCaption", False, "Fantasy", 3, 2.5)
-    print(audio_book.__str__())
+    print(audio_book)
 
     magazine = Magazine("MagazineCaption", "Documental", "Ukraine History")
-    print(magazine.__str__())
+    print(magazine)
 
 
 if __name__ == '__main__':
